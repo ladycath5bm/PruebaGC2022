e254e0652cd2ea3a37848d6e80fc8130f1b156f9
ladycath5bm <leidycath.bm@gmail.com>
2022-06-08 13:07:38 -0500

    feat: 🎸 conection with js

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Gce_caracteristicasController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/addOne', [Gce_caracteristicasController::class, 'addOne'])
+    ->name('characteristic.addOne');
+Route::get('/getOne/{id}', [Gce_caracteristicasController::class, 'getOne'])
+    ->name('characteristic.getOne');
+Route::get('/getAll', [Gce_caracteristicasController::class, 'getAll'])
+    ->name('characteristic.getAll');
+
